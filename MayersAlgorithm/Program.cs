2b8c4bc2eab26fc6b7ead2265f0e6a52929d9da5
informant <artemiy.solodkin@mail.ru>
2023-12-10 17:43:07 +0500

    little readiment

--- MayersAlgorithm/Program.cs
+++ MayersAlgorithm/Program.cs
@@ -4,15 +4,15 @@
     {
         static int[,] LCSTable(char[] startWord, char[] endWord)
         {
-            int n = endWord.Length;
+            int rowCount = endWord.Length;
 
-            int m = startWord.Length;
+            int columnCount = startWord.Length;
 
-            int[,] C = new int[n + 1 , m + 1];
+            int[,] C = new int[rowCount + 1 , columnCount + 1];
 
-            for (int i = 1; i <= n; i++)
+            for (int i = 1; i <= rowCount; i++)
             {
-                for (int j = 1; j <= m; j++)
+                for (int j = 1; j <= columnCount; j++)
                 {
                     if (startWord[j - 1] == endWord[i - 1])
                     {
@@ -27,35 +27,39 @@
 
             return C;
         }
-        static void PrintDiff(int[,] C, char[] startWord, char[] endWord, int lineIndex, int columnIndex)
+        
+        static void PrintDiff(int[,] C, char[] startWord, char[] endWord, int i, int j)
         {
-            if (lineIndex-1 >= 0 && columnIndex-1 >= 0 && startWord[columnIndex - 1] == endWord[lineIndex-1])
-            {
-                PrintDiff(C, startWord, endWord, lineIndex - 1, columnIndex - 1);
-                Console.WriteLine("= " + endWord[lineIndex - 1]);
+            snakeIndexes[i, j] = true;
+            if (i-1 >= 0 && j-1 >= 0 && startWord[j - 1] == endWord[i-1])
+            {   
+                PrintDiff(C, startWord, endWord, i - 1, j - 1);
+                Console.WriteLine("= " + endWord[i - 1]);
             }
-            else if (columnIndex > 0 && (lineIndex == 0 || C[lineIndex, columnIndex - 1] > C[lineIndex - 1, columnIndex]))
+            else if (j > 0 && (i == 0 || C[i, j - 1] > C[i - 1, j]))
             {
-                PrintDiff(C, startWord, endWord, lineIndex, columnIndex - 1);
-                Console.WriteLine("- " + startWord[columnIndex-1]);
+                PrintDiff(C, startWord, endWord, i, j - 1);
+                Console.WriteLine("- " + startWord[j-1]);
             }
-            else if (lineIndex > 0 && (columnIndex == 0 || C[lineIndex, columnIndex - 1] <= C[lineIndex - 1, columnIndex]))
+            else if (i > 0 && (j == 0 || C[i, j - 1] <= C[i - 1, j]))
             {
-                PrintDiff(C, startWord, endWord, lineIndex - 1, columnIndex);
-                Console.WriteLine("+ " + endWord[lineIndex - 1]);
+                PrintDiff(C, startWord, endWord, i - 1, j);
+                Console.WriteLine("+ " + endWord[i - 1]);
             }
             else
             {
-                Console.Write("");
+                return;
             }
         }
+        static bool[,] snakeIndexes;
         static void Main()
         {
             char[] startWord = "BACAAC".ToCharArray();
             char[] endWord = "CBCBAB".ToCharArray();
 
             int[,] result = LCSTable(startWord, endWord);
-
+            snakeIndexes = new bool[endWord.Length+1, startWord.Length+1];
+            
             Console.Write("    ");
             for(int k=0; k < startWord.Length; k++)
             {    
@@ -84,6 +88,39 @@
             int n = endWord.Length;
             int m = startWord.Length;
             PrintDiff(result, startWord, endWord, n, m);
+
+            Console.Write("    ");
+            for (int k = 0; k < startWord.Length; k++)
+            {
+                Console.Write(startWord[k] + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < endWord.Length + 1; i++)
+            {
+                if (i >= 1)
+                {
+                    Console.Write(endWord[i - 1] + " ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 0; j < startWord.Length + 1; j++)
+                {
+                    if (snakeIndexes[i, j])
+                    {   
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(result[i, j] + " ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.Write(result[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
         } 
     }
 
